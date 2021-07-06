de3d5de1d56585c3a31d59f1e57cc0f816e28dbd
Laura Kapitza <laura.kapitza@gmail.com>
2021-07-06 20:42:28 +0200

    done

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,9 @@ import React from 'react';
 class SearchBar extends React.Component {
     render() {
         return(
-            <div>
+            <div className="searchbar">
                 <label>
-                    Search
-                    <input type='search' name='search' value={this.props.queryValue} onChange={(event) => {this.props.queryHandler(event.target)}}></input>
+                    <input type='search' name='search' value={this.props.queryValue} onChange={(event) => {this.props.queryHandler(event.target)}} placeholder="Search..."></input>
                 </label>
                 <label>
                     <input type='checkbox' value={this.props.inStockValue} onChange={event => {this.props.queryHandler(event.target)}}></input>
